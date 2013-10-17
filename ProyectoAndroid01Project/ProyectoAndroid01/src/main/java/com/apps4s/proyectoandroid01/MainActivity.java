ade9396de68e54ae2f69fbfa71841312fe69c061
Henry AT <yiperu@gmail.com>
2013-10-16 19:21:01 -0500

    Adicionamos el archivo colors.xml para crear los colores, eh ingresamos mas controles para seguir operando con los diversos controladores

--- ProyectoAndroid01Project/ProyectoAndroid01/src/main/java/com/apps4s/proyectoandroid01/MainActivity.java
+++ ProyectoAndroid01Project/ProyectoAndroid01/src/main/java/com/apps4s/proyectoandroid01/MainActivity.java
@@ -3,6 +3,8 @@ package com.apps4s.proyectoandroid01;
 import android.os.Bundle;
 import android.app.Activity;
 import android.view.Menu;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends Activity {
 
@@ -10,8 +12,16 @@ public class MainActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-    }
 
+        TextView mio = (TextView)findViewById(R.id.textView);
+        mio.setBackgroundColor(0x0000FF);
+        mio.setText(R.string.htmls);
+        //mio.setText("Para que estan los Chofere..");
+
+        Button btn = (Button)findViewById(R.id.boton001);
+        btn.setText(R.string.cadenaBoton);
+
+    }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
